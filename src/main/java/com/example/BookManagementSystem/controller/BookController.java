18eba47071ac06e4fdc5ddd29a2d54180b6c01b5
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2025-05-04 16:21:45 +0530

    SpringBoot: started working on controller class

--- src/main/java/com/example/BookManagementSystem/controller/BookController.java
+++ src/main/java/com/example/BookManagementSystem/controller/BookController.java
@@ -1,4 +1,23 @@
 package com.example.BookManagementSystem.controller;
 
+import com.example.BookManagementSystem.entity.Book;
+import com.example.BookManagementSystem.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/book/v1")
 public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    @PostMapping("/addbook")
+    public ResponseEntity addBook(@RequestBody Book book) {
+
+    }
 }
